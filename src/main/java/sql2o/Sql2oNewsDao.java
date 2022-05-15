4540050721752efc2efdeedb6cd553330d4a35ff
SMetto20 <smetto20@gmail.com>
2022-05-15 09:36:42 +0300

    adds news dao methods and tests

--- src/main/java/sql2o/Sql2oNewsDao.java
+++ src/main/java/sql2o/Sql2oNewsDao.java
@@ -6,6 +6,9 @@ import Models.Department;
 import Models.News;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
 
 public class Sql2oNewsDao implements NewsDao {
     private final Sql2o sql2o;
@@ -21,6 +24,35 @@ public class Sql2oNewsDao implements NewsDao {
 //                    .addParameter("health", this.health)
                     .bind(news)
                     .executeUpdate();
+        }
+    }
+    public static List<News> getAll() {
+        String sql = "SELECT * FROM News";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(News.class);
+        }
+
+    }
+    public void update (News news) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE news SET name = :name WHERE id=:id";
+            con.createQuery(sql)
+                    .bind(news)
+                    .executeUpdate();
+        } catch (Sql2oException e){
+            System.out.println(e.getMessage());
+
+        }
+    }
+    public void deleteByGeneral(String general){
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE  FROM news WHERE general=:general";
+            con.createQuery(sql)
+                    .addParameter("general", general)
+                    .executeUpdate();
+        } catch (Sql2oException e){
+            System.out.println(e.getMessage());
+
         }
     }
 }
--- src/test/java/sql2o/Sql2oNewsDaoTest.java
+++ src/test/java/sql2o/Sql2oNewsDaoTest.java
@@ -1,4 +1,54 @@
 package sql2o;
 
+import Dao.NewsDao;
+import Models.Department;
+import Models.News;
+import Models.Users;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Sql2o;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Sql2oNewsDaoTest {
+    String connect = "jdbc:postgresql://localhost:5432/news";
+    Sql2o sql2o = new Sql2o(connect,"postgres","123");
+    private Sql2oNewsDao newsDao = new Sql2oNewsDao(sql2o);
+    private Sql2oDepartmentDao  DepartmentDao = new Sql2oDepartmentDao(sql2o);
+    private  Sql2oUsersDao UsersDao = new Sql2oUsersDao(sql2o);
+
+    @Test
+    public void  add_addsNews_true(){
+        Department department = new Department("fishing", "wildlife",1, 2);
+        DepartmentDao.add(department);
+        News news = new News("general", "music",department.getId());
+        newsDao.add(news);
+        assertEquals("general",news.getGeneral());
+
+    }
+    @Test
+    public void  geAll_returnsAllNews_true(){
+       News news = new News("world news", "music", 3);
+       News news2 = new News("weather", "business", 2);
+        newsDao.add(news);
+        newsDao.add(news2);
+        int expected = 19;
+        Assert.assertEquals(expected, newsDao.getAll().size());
+    }
+    @Test
+    public void update_updatesNewsDetails_true(){
+        News news = new News("world news", "music", 3);
+        newsDao.add(news);
+        news.setDepartmental("History");
+        newsDao.update(news );
+        Assert.assertEquals("History", news.getDeparmental());
+    }
+    @Test
+    public void delete_deletesNews_true(){
+        News news = new News ("Fashion", "weather", 2);
+        newsDao.add(news);
+        newsDao.deleteByGeneral(news.getGeneral());
+        Assert.assertEquals(29, DepartmentDao.getAll().size());
+    }
 }
+
